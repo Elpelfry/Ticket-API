897237a22b0ca360990eb6188d5a5b5209ed1566
ElPelfry <delfry_paulino@ucne.edu.do>
2024-10-04 20:52:18 -0400

    models and interfaces

---
+++ Shared/Interfaces/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Shared.Interfaces;
+
+public interface IGenericService<T>
+{
+    Task<T> Add(T type);
+    Task<T> Get(int id);
+    Task<bool> Update(T type);
+    Task<bool> Delete(int id);
+    Task<List<T>> GetAll();
+}
---
+++ Shared/Models/Tickets.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.Models;
+
+public class Tickets
+{
+    [Key]
+    public int TicketId { get; set; }
+    [Required(ErrorMessage = "El campo fecha es obligatorio")]
+    public DateTime Fecha { get; set; }
+   
+    [Required(ErrorMessage = "Requerido")]
+    [ForeignKey("Prioridades")]
+    public int PrioridadId { get; set; }
+    
+    [Required(ErrorMessage = "Requerido")]
+    [ForeignKey("Clientes")]
+    public int ClienteId { get; set; }
+    
+    [Required(ErrorMessage = "Requerido")]
+    [ForeignKey("Sistemas")]
+    public int SistemaId { get; set; }
+
+    [Required(ErrorMessage = "El campo Solicitado por es obligatorio")]
+    [RegularExpression("^[a-zA-Z ]+$", ErrorMessage = "Solo debe contener letras.")]
+    public string? Solicitadopor { get; set; }
+
+    [Required(ErrorMessage = "El campo Asunto es obligatorio")]
+    public string? Asunto { get; set; }
+
+    [Required(ErrorMessage = "El campo Descripción es obligatorio")]
+    public string? Descripcion { get; set; }
+
+    [ForeignKey("TicketId")]
+    public ICollection<TicketsDetalle> TicketsDetalle { get; set; } = new List<TicketsDetalle>();
+}
